6fcad703ee0fd0508e464d3677e695f996ac691e
ahnaf005 <ahnaffaisalalavi@gmail.com>
2018-04-27 19:26:54 +0600

    first commit

---
+++ FactoryMethod/src/factorymethod/FactoryMethod.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorymethod;
+interface Coffee{
+    void prepare();
+}
+public class FactoryMethod {
+    public static void main(String[] args) {
+        
+    }
+    
+}
